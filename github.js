16ecd83d19c77fef4d342cd235367d2a8118e7aa
Filip Kolev <filipco74@gmail.com>
2023-01-29 16:12:13 +0100

    First commit

---
+++ github.js
@@ -0,0 +1,26 @@
+class Github {
+  constructor() {
+    this.client_id = "017aef22ad1dad22a346";
+    this.client_secret = "eeb84608e119eb9e554cc7b5b008202bbcaa1e1a";
+    this.repos_count = 5;
+    this.repos_sort = "created: asc";
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(
+      `https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`
+    );
+
+    const repoResponse = await fetch(
+      `https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`
+    );
+
+    const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
+
+    return {
+      profile,
+      repos,
+    };
+  }
+}
